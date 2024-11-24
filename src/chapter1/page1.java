7a30417a07f5ab46c22b3d922c8dc279104a54e1
nargiz <nargiz.d.aslanova@gmail.com>
2024-11-24 11:39:32 -0500

    added page1 file

--- src/chapter1/page1.java
+++ src/chapter1/page1.java
@@ -4,5 +4,6 @@ public class page1 {
     public static void main(String[] args) {
         System.out.println("Feyruz added this line");
         System.out.println("Feyruz added another line of code");
+        System.out.println("Feyruz added this as for his user story");
     }
 }
